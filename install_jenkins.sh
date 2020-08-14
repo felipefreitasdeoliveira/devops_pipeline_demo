863a7085b31b4beb5b0d5fa8ba1c8d4752cdbd02
Felipe Freitas <31215284+felipefreitasdeoliveira@users.noreply.github.com>
2020-08-14 17:27:23 -0300

    Create install_jenkins.sh

---
+++ install_jenkins.sh
@@ -0,0 +1,12 @@
+#!bash
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo yum install jenkins java-1.8.0-openjdk-devel
+
+sudo systemctl start jenkins
+
+sudo systemctl status jenkins
+
+
