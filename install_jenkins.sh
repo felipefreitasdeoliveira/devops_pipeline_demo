ce68ba065436cb9eaa68fc4f5a994354ae20e7f3
Felipe Freitas <31215284+felipefreitasdeoliveira@users.noreply.github.com>
2020-08-14 22:16:29 -0300

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -4,8 +4,7 @@
 yum -y install git maven docker vim telent curl wget 
 
 # Jenkins
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum -y upgrade
 sudo yum -y install jenkins java-1.8.0-openjdk-devel
